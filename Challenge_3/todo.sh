3a0d7bffd113d0586e6c63e80f28267f73815200
atulbhingarde <atul.bhingarde@ge.com>
2019-08-24 23:54:03 -0400

    final take on todo list

--- Challenge_3/todo.sh
+++ Challenge_3/todo.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 find ../ -type f -name Todo*.zip -exec unzip -u '{}' \; 
-find Todos -type f -path "*todos*" 
+tempfile=temp.txt
+for Files in `find Todos -type f -path "*todos*"` ; do
+ name=`echo ${Files} | awk -F"/" '{ print $2 }' `
+ cat ${Files} | awk -v thisName=${name} '{ print thisName"|"$0 }'
+done > ${tempfile} 
+awk -F"|" '{ if ( tolower($2) ~/done/ ) 
+	      { done[$1]++ } 
+             else { pending[$1]++ } } 
+	   END{ print "Done:" ; for(j in done) print j": " done[j] ; \
+		print "\nTo Still do:" ; for(j in pending) print j": "pending[j] }' ${tempfile}
